cf12cd4a082b1b1ea78b4c0d4a8a0e0df792cbf9
CrhistoferVera <cristofervera110@gmail.com>
2026-01-20 10:23:56 -0400

    complete profile dto

--- src/users/dto/complete-profile.dto.ts
+++ src/users/dto/complete-profile.dto.ts
@@ -1,8 +1,13 @@
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { Country } from "./create-user.dto";
 
 export class CompleteProfileDto {
 
+    @IsString()
     password: string;
-    phoneNumber: string;
+    @IsOptional()
+    @IsString()
+    phoneNumber?: string;
+    @IsEnum(Country)
     country: Country;
 }
