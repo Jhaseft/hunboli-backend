6a1754fca4cb0483fbafee4807089008c48fef24
CrhistoferVera <cristofervera110@gmail.com>
2026-02-10 15:56:30 -0400

    Add sendCodeVerification

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -13,10 +13,21 @@ export class MailService {
       subject: 'Recuperación de contraseña',
       // Opción A: HTML directo (rápido para probar)
       html: `
-        <h1>Recuperar contraseña</h1>
+        <h1>Recuperar contraseña - HUNBOLI</h1>
         <p>Haz clic en el siguiente enlace para cambiar tu contraseña:</p>
         <a href="${url}">Cambiar contraseña</a>
         <p>Este enlace expira en 1 hora.</p>
+      `,
+    });
+  }
+
+  async send6DigitCode(email: string, code: string) {
+    await this.mailerService.sendMail({
+      to: email,
+      subject: 'Código de verificación - HUNBOLI',
+      html: `
+        <h1>Código de verificación</h1>
+        <p>Tu código de verificación es: <strong>${code}</strong></p>
       `,
     });
   }
